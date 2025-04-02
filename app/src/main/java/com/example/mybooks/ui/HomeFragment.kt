7f41360d7f49c2ae5c4ae474f15c01944c192acf
Heitor <faradheitor@gmail.com>
2025-04-02 16:48:33 -0300

    mybooks finalizado

--- app/src/main/java/com/example/mybooks/ui/HomeFragment.kt
+++ app/src/main/java/com/example/mybooks/ui/HomeFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mybooks.R
 import com.example.mybooks.databinding.FragmentHomeBinding
-import com.example.mybooks.viewmodels.HomeViewModel
+import com.example.mybooks.helper.BookConstants
+import com.example.mybooks.ui.adapter.BookAdapter
+import com.example.mybooks.ui.listener.BookListener
+import com.example.mybooks.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +21,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: HomeViewModel by viewModels()
+    private val adapter: BookAdapter = BookAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,11 +31,46 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        binding.recyclerviewBooks.layoutManager = LinearLayoutManager(context)
+        binding.recyclerviewBooks.adapter = adapter
+
+        attachListener()
+
+        setObservers()
+
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getAllBooks()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun attachListener() {
+        adapter.attachListener(object : BookListener {
+            override fun onClick(id: Int) {
+                val bundle = Bundle()
+                bundle.putInt(BookConstants.KEY.BOOK_ID, id)
+
+                findNavController().navigate(R.id.navigation_details, bundle)
+            }
+
+            override fun onFavoriteClick(id: Int) {
+                viewModel.favorite(id)
+                viewModel.getAllBooks()
+            }
+        })
+    }
+
+    fun setObservers() {
+        viewModel.books.observe(viewLifecycleOwner) {
+            adapter.updateBooks(it)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/mybooks/ui/viewholder/BookViewHolder.kt
@@ -0,0 +1,48 @@
+package com.example.mybooks.ui.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mybooks.R
+import com.example.mybooks.databinding.ItemBookBinding
+import com.example.mybooks.entity.BookEntity
+import com.example.mybooks.ui.listener.BookListener
+
+class BookViewHolder(private val item: ItemBookBinding, private val listener: BookListener) :
+    RecyclerView.ViewHolder(item.root) {
+
+    fun bind(book: BookEntity) {
+        item.textviewTitle.text = book.title
+        item.textviewAuthor.text = book.author
+        item.textviewGenre.text = book.genre
+
+        item.textviewTitle.setOnClickListener { listener.onClick(book.id) }
+        item.imageviewFavorite.setOnClickListener { listener.onFavoriteClick(book.id) }
+
+        setGenreBackgroun(book.genre)
+
+        updateFavoriteIcon(book.favorite)
+    }
+
+    private fun updateFavoriteIcon(favorite: Boolean) {
+        if (favorite) {
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite)
+        } else {
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite_empity)
+        }
+    }
+
+    private fun setGenreBackgroun(genre: String) {
+        when (genre) {
+            "Terror" -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_red)
+            }
+            "Fantasia" -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_fantasy)
+            }
+            else -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_teal)
+            }
+        }
+    }
+
+}
